d5895a73a626a57db5062ad6d36ada6cea9cf986
skr0nks <108427178+skr0nks@users.noreply.github.com>
2022-09-13 06:31:25 +0000

    Update bot_tg.py

--- bot_tg.py
+++ bot_tg.py
@@ -1,60 +1,49 @@
-
-import requests
 import telebot
-from auth_data_bot import token
-import datetime
-from telebot import types
 
 
-def get_data():
-    req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
-    response = req.json()
-    sell_price = response["btc_usd"]["sell"]
-    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}")
 
-def telegram_bot(token):
-    bot = telebot.TeleBot(token)
-
-    @bot.message_handler(commands=['start'])
-    def start_message(message):
-        bot.send_message(message.chat.id, 'hello')
-
-        																																				bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
-   
-
-    @bot.message_handler(content_types=['text'])
-    def send_text(message):
-        if message.text.lower() == "price":
-            try:
-                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
-                response = req.json()
-                data = datetime.datetime.now()
-                sell_price = response["btc_usd"]["sell"]
-                bot.send_message(
-                    message.chat.id,
-                    '{}\nSell BTC price:{}'.format(sell_price,data)
-                    
-                    # \nSell BTC price: {sell_price}"
-
-# f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\n
+token = 'your token'
+bot = telebot.TeleBot(token)
 
 
-                    # bot.send_message(message.chat.id, "Температура в {}: {} C".format(city, temp))
-                    # bot.send_message(message.chat.id, 'Температура в ', city, ': ',temp , '°C')
-                )
-            except Exception as ex:
-                print(ex)
-                bot.send_message(
-                    message.chat.id,
-                    'wrong'
-                )
 
 
-    bot.polling()
+@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['test'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, я помогу тебе решить, что писать {name of your friend} из {name} ))))) ')
+    bot.send_message(message.chat.id, 'прислушайся ко мне.. вот, можешь почитать статю про него : {article name} ')
+
+
+
+
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Пивка для рывка?', callback_data=3))
+    markup.add(telebot.types.InlineKeyboardButton(text='Is London the capital of Great Britain?', callback_data=4))
+    markup.add(telebot.types.InlineKeyboardButton(text='Смотрел обзор Бэда?', callback_data=5))
+    markup.add(telebot.types.InlineKeyboardButton(text='Вкусно и точка,или невкусно и запятая(', callback_data=6))
+    bot.send_message(message.chat.id, text="Какую именно цитату ты бы отпрвила?", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+
+    bot.answer_callback_query(callback_query_id=call.id, text='Отличный выбор, так и пиши')
+    answer = ''
+    if call.data == '3':
+        answer = 'Доступные варианты: Жигулевское, Козел темный, Козел светлый'
+        
+    elif call.data == '4':
+        answer = 'Не очень... : {name of the article}}'
+    elif call.data == '5':
+        answer = 'Да, он смотрел, вот ссылка : {name of the article}}'
+    elif call.data == '6':
+        answer = 'Вкусно блин и точка нафиг'
+
+
+    bot.send_message(call.message.chat.id, answer)
 
 
 
 
 
-if __name__=="__main__":
-    telegram_bot(token)
+bot.polling(non_stop=True)
