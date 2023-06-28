6322526ba6867334f04002db4814b209b0b70764
skr0nks <108427178+skr0nks@users.noreply.github.com>
2023-06-28 23:30:14 +0300

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -8,10 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import glob
 
-# элементы вэбрайвера
-driver = webdriver.Chrome(ChromeDriverManager().install())
-opts = Options()
-opts.add_experimental_option("detach", True)
+
 
 def get_video():
     try:
